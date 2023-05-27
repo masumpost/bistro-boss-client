6abef6ed26fe323623eb1d4d19854a5ca1cb900a
md-masum <masum.nextu@gmail.com>
2023-05-27 22:03:51 +0600

    dynamic route system added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import ChefRecommends from "../../ChefRecommends/ChefRecommends";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
@@ -10,6 +11,9 @@ import Featured from "./Featured/Featured";
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss - Home</title>
+            </Helmet>
            <Banner></Banner>
            <Category></Category>
            <ChefService></ChefService>
--- src/Pages/Menu/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu/Menu.jsx
@@ -1,7 +1,11 @@
+import { Helmet } from "react-helmet-async";
 
 const Menu = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss - Menu</title>
+            </Helmet>
             <h1>This is menu</h1>
         </div>
     );
